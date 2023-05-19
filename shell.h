8813decdd54a4a9cd7eff50fb540ab5b4c315bf4
kelechimaduka <kelechimaduka9@gmail.com>
2023-05-19 03:21:56 -0700

    header file

---
+++ shell.h
@@ -0,0 +1,62 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFSIZE 1024
+extern char **Envi;
+/**
+ * strict envi_t - linked list
+ * @str: path
+ * @len: length
+ * @next_node: next node
+ */
+typedef struct envi_t
+{
+	char *str;
+	unsigned int len;
+	struct envi_t *next_node;
+}
+env;
+
+/**
+ * struct builtin_cmd - struct for builtin commands
+ * @cmd_: commands
+ * @function: function
+ */
+typedef struct builtin_cmd
+{
+	char *cmd_;
+	int (*function)();
+}
+builtin_c;
+
+int (*builTin(char *cmd))();
+int exitTING(char **token, env *environ, char *buffer);
+int _env(char **token, env *environment);
+int cdD(char **token);
+int set_user(char **token);
+int stren(void);
+int past(void);
+int barbie(void);
+env *list_path(void);
+env *envi_L_list(void);
+char *search_(char *cmd, env *environ);
+char *get_env(const char *nom);
+int set_env(const char *nom, const char *value, int overwrite);
+env *addnode(env **head, char *str, unsigned int len);
+void freelist(env *head);
+void executee(char *argv[], env *environ);
+void *re_alloc(char *ptr, unsigned int old, unsigned int new_size);
+void mem_set(char *str, int fill, int x);
+void mem_cpy(char *dest, char *src, unsigned int bytes);
+
+#endif
