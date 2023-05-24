fdb97f4b8fd6c64b075abe7b2f9ee84ca1bd898c
kelechimaduka <kelechimaduka9@gmail.com>
2023-05-24 04:46:46 -0700

    list operations

---
+++ list_op.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+/**
+ * addnode - adds new node
+ * @head: start
+ * @str: string
+ * @len: length of string
+ * Return: Success
+ */
+env *addnode(env **head, char *str, unsigned int len)
+{
+	env *new, *ju;
+	char *str2;
+
+	if (str == NULL)
+		return (NULL);
+	str2 = str_up(str);
+	if (str2 == NULL)
+		return (NULL);
+	new = malloc(sizeof(env));
+	if (new == NULL)
+		return (NULL);
+	new->str = str2;
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	ju = *head;
+	while (ju->next != NULL)
+		ju = ju->next;
+	ju->next = new;
+	return (*head);
+}
+/**
+ * freelist - frees list
+ * @head: start
+ */
+void freelist(env *head)
+{
+	if (head == NULL)
+		return;
+	freelist(head->next);
+	free(head->str);
+	free(head);
+}
