a34c5ac09ae2e2d03ea36c2870b1040bc85bbac6
kelechimaduka <kelechimaduka9@gmail.com>
2023-05-24 03:44:53 -0700

    executor

---
+++ exec.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+/**
+ * executee - executes a command
+ * @argv: array of tokens
+ * @environ: path
+ * Return: Success
+ */
+void executee(char *argv[], env *environ)
+{
+	pd_ stat_1 = 0;
+	char *absolutepath;
+
+	absolutepath = search_(argv[0], environ);
+	if (!absolutepath)
+	{
+		perror("command not found\n");
+		return;
+	}
+
+	switch (stat_1 = fork())
+	{
+		case -1:
+			perror("fork failed\n");
+			break;
+		case 0:
+			if (execve(absolutepath, argv, Envi) == -1)
+				perror("execution failed\n");
+			break;
+		default:
+			free(absolutepath);
+			if (wait(NULL) == -1)
+				perror("wait failed\n");
+			break;
+	}
+}
